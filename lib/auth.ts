6cbe6fd8a5c2709ecb1f3d6b6235a3fd46aadff0
Min Wai <minwaiphyo11@gmail.com>
2025-10-25 15:48:30 +0800

    changed session lifespan to 30mins

--- lib/auth.ts
+++ lib/auth.ts
@@ -79,7 +79,7 @@ export const authOptions: NextAuthOptions = {
   ],
   session: {
     strategy: "jwt",
-    maxAge: 30 * 24 * 60 * 60, // 30 days
+    maxAge: 30 * 60, // 30 minutes (in seconds)
   },
   callbacks: {
     async jwt({ token, user }) {
