929ff36fb34101376eedbac75493ae24e766d32f
Min Wai Phyo <98584889+minwaiphyo@users.noreply.github.com>
2026-02-09 12:23:22 +0800

    Fix bug

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
-// export const supabase = createClient("https://feowsnofuagwjwusfbyq.supabase.co", "sb_secret_DpvgQiiZlSs4WUGPGzYFMw_M415wZtW")
 
 export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
